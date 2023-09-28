fd6a12149d44c33efdfa684422b9dad51e0e3af3
JoYuLiJe <141531126+JoYuLiJe@users.noreply.github.com>
2023-09-27 19:59:36 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import HelloWorld from './HelloWorld.jsx';
+import ClickCounter from './ClickCounter';
 
 function App() {
   return (
     <div>
       <h1>React Demos 9/27</h1> 
+      <HelloWorld/>
+      <ClickCounter/>
     </div>
   );
 }
---
+++ src/ClickCounter.jsx
@@ -0,0 +1,34 @@
+//Create a functional component called Click Counter.
+// It should have a paragraph tag that displays the current 'count'
+// and a button element that increments the count whenever it is clicked
+// Import and use this component in app.jsx
+
+import { useState } from "react";
+
+const ClickCounter = () => {
+    /* UseState is a function that returns an array that conatains two things;
+    a variable and a function whose only job is to change that variable (or SET it)
+    the parameter you give useState sets the initial value of the variable
+    */
+
+    const [count, setCount] = useState(0);
+
+    // const = count + 5; BAD
+    // setCount(count + 5); GOOD
+
+    return (
+        <>
+            <h2>Exercise 2</h2>
+            <p>The current count is: {count}</p>
+            <button styles={{color: 'blue'}} onCLick={() => setCount(count + 1)}>Increment the count!</button>
+            <p>Add button thta resets count</p>
+            <button onClick={() => setCount(0)}>Reset counter to zero!</button>
+
+
+            
+        </>
+
+    )
+}
+
+export default ClickCounter;
